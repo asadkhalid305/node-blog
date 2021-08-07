cb4f24c348b24ee6c6c7a3a4d2f1360f61b9a495
Asad Ullah Khalid <asad.khalid@securiti.ai>
2021-08-07 16:50:41 +0500

    added http server in index.js file

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+const Http = require("http");
 const express = require('express');
 const morgan = require('morgan');
 const mongoose = require('mongoose');
@@ -6,26 +7,43 @@ const blogRoutes = require('./routes/blogs');
 // express app
 const app = express();
 
-// mongoDbURI
-const mongoDbUri = 'mongodb+srv://aahad:aahadtest1234@expressblogsite.y9kmb.mongodb.net/expressblogdatabase?retryWrites=true&w=majority'
-
-// connect mongoDb with mongoose
-mongoose.connect(mongoDbUri).then(res => {
-  console.log('-----Connected to DB!-----')
-  // listen for requests
-  app.listen(process.env.PORT || 3000);
-}).catch(err => {
-  console.log('-----Unable to connect to DB!-----')
-})
-
 // register view engine
 app.set('view engine', 'ejs');
 // use middleware for static files
 app.use(express.static('public'));
 // url encoding middleware
-app.use(express.urlencoded());
+app.use(express.urlencoded({ extended: true }))
 // use morgan to log
 app.use(morgan('dev'));
+
+//creating server
+const server = Http.createServer(app);
+const port = process.env.PORT || 3000;
+
+// mongoDbURI
+const mongoDbUri = 'mongodb+srv://aahad:aahadtest1234@expressblogsite.y9kmb.mongodb.net/expressblogdatabase?retryWrites=true&w=majority'
+
+// connect mongoDb with mongoose
+mongoose.connect(mongoDbUri, {
+  useNewUrlParser: true,
+  useFindAndModify: false,
+  useUnifiedTopology: true
+})
+
+const db = mongoose.connection;
+
+//checking for errors while connecting
+db.on("error", (err) => {
+  console.error(`Error in connecting MongoDB:\n ${err}`);
+});
+
+//checking if the connection has been established
+db.on("connected", () => {
+  console.log("MongoDB is up and running!");
+  server.listen(port, () => {
+    console.log(`Server is up and running @${port}`);
+  });
+});
 
 app.get('/', (req, res) => {
   res.redirect('/blogs');
